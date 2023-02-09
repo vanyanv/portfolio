e34e26b6df5d7e310e02996e1ac29d5503e564bf
Vardan <vardan.vanyan@gmail.com>
2023-02-08 18:14:29 -0800

    added chnages to main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -250,9 +250,7 @@ export default function Home({ articles }) {
   return (
     <>
       <Head>
-        <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
-        </title>
+        <title>Vardan Vanyan - Software Engineer</title>
         <meta
           name="description"
           content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
@@ -261,13 +259,16 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software Engineer
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            Experienced in building full-stack JavaScript applications using
+            React, Express, Node, PostgreSQL/NoSQL Databases, Next.js,
+            TypeScript, and Electron. An empathetic engineer, who is passionate
+            contributor to open-source content, and co-creator of MicrObserv, an
+            open-source real-time and event-driven observability tool for
+            microservices, that provides valuable information to solve issues as
+            soon as they arise.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
