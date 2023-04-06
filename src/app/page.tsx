9cae48acfe198d7bf543f5bea269e48aaf2a6d16
Vardan <vardan.vanyan@gmail.com>
2023-04-06 14:25:59 -0700

    added projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Projects />
     </>
   );
 }
