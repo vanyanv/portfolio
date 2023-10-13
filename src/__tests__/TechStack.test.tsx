b8c1c82d0a54eeda19c2b932e28c1c08c3f13334
Vardan <vardan.vanyan@gmail.com>
2023-10-13 11:41:29 -0700

    added testing

---
+++ src/__tests__/TechStack.test.tsx
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import TechStack from '@/components/TechStack';
+
+
+test('Display Tech Images', () => {
+    const  techStack = render(<TechStack />);
+
+    
+});
