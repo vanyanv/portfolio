6e616412bbff741a57e4fbf7502a628ad07b0e12
Vardan <vardan.vanyan@gmail.com>
2023-10-24 14:28:55 -0700

    function framer motion animations for hero and tech stack

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,22 +1,39 @@
 'use client';
 import React, { useEffect, useRef } from 'react';
-import { motion, useInView, useAnimate } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { motion, useAnimation } from 'framer-motion';
 
 type Props = {
   children: JSX.Element;
   width?: 'fit-content' | '100%';
+  className?: string;
 };
 
-export default function Reveal({ children, width = 'fit-content' }: Props) {
+export default function Reveal({
+  children,
+  width = 'fit-content',
+  className,
+}: Props) {
+  const [ref, inView, entry] = useInView({ threshold: 0.1 });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      mainControls.start('visible');
+    } else {
+      mainControls.start('hidden');
+    }
+  }, [inView, mainControls]);
+
   return (
-    <div style={{ position: 'relative', width, overflow: 'hidden' }}>
+    <div ref={ref} className={`relative ${width} overflow-hidden ${className}`}>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
         }}
         initial='hidden'
-        animate='visible'
+        animate={mainControls}
+        transition={{ duration: 0.4, delay: 0.5 }}
       >
         {children}
       </motion.div>
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Reveal from './Reveal';
 const stats = [
   { id: 1, name: 'HTML', value: `/Icons/HTML.svg` },
   { id: 2, name: 'CSS', value: '/Icons/CSS.svg' },
@@ -13,43 +14,46 @@ const stats = [
     name: 'Typescript',
     value: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg',
   },
-  { id: 4, name: 'React', value: '/Icons/React.svg' },
-  { id: 5, name: 'Redux', value: '/Icons/Redux.svg' },
-  { id: 6, name: 'Nextjs', value: '/Icons/Nextjs.svg' },
-  { id: 7, name: 'Node', value: '/Icons/Node.svg' },
-  { id: 8, name: 'Postgres', value: '/Icons/Postgres.svg' },
-  { id: 9, name: 'MongoDB', value: '/Icons/MongoDB.svg' },
-  { id: 10, name: 'TailwindCss', value: '/Icons/Tailwindcss.svg' },
-  { id: 11, name: 'Jest', value: '/Icons/Jest.svg' },
+  { id: 5, name: 'React', value: '/Icons/React.svg' },
+  { id: 6, name: 'Redux', value: '/Icons/Redux.svg' },
+  { id: 7, name: 'Nextjs', value: '/Icons/Nextjs.svg' },
+  { id: 8, name: 'Node', value: '/Icons/Node.svg' },
+  { id: 9, name: 'Postgres', value: '/Icons/Postgres.svg' },
+  { id: 10, name: 'MongoDB', value: '/Icons/MongoDB.svg' },
+  { id: 11, name: 'TailwindCss', value: '/Icons/Tailwindcss.svg' },
+  { id: 12, name: 'Jest', value: '/Icons/Jest.svg' },
 ];
 
 export default function TechStack() {
   return (
     <div id='technologies' className='py-24 sm:py-32 min-h-screen'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8 '>
-        <div className='mx-auto max-w-2xl lg:max-w-none'>
+        <div className='mx-auto  max-w-2xl lg:max-w-none'>
           <div className='text-center'>
             <h2 className='text-xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
               {'<Technologies/>'}
             </h2>
           </div>
+
           <dl className='mt-16 grid grid-cols-2 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4'>
             {stats.map((stat) => (
-              <div
-                key={stat.id}
-                className='flex flex-col items-center bg-gray-400/5 p-8 hover:bg-gray-400/10 transition duration-300 ease-in-out rounded-2xl'
-              >
-                <dt className='text-sm font-semibold leading-6 text-gray-600'>
-                  {stat.name}
-                </dt>
-                <Image
-                  className='order-first text-3xl font-semibold tracking-tight text-gray-900'
-                  src={stat.value}
-                  alt={stat.name}
-                  width={50}
-                  height={50}
-                />
-              </div>
+              <Reveal key={stat.id}>
+                <div
+                  key={stat.id}
+                  className='flex flex-col items-center bg-gray-400/5 p-8 hover:bg-gray-400/10 transition duration-300 ease-in-out rounded-2xl'
+                >
+                  <dt className='text-sm font-semibold leading-6 text-gray-600'>
+                    {stat.name}
+                  </dt>
+                  <Image
+                    className='order-first text-3xl font-semibold tracking-tight text-gray-900'
+                    src={stat.value}
+                    alt={stat.name}
+                    width={50}
+                    height={50}
+                  />
+                </div>
+              </Reveal>
             ))}
           </dl>
         </div>
