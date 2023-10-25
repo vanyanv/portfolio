0fbbdee06889edc437e3106b77e472aec2b68fca
Vardan <vardan.vanyan@gmail.com>
2023-10-25 12:33:50 -0700

    nextjs config changes for images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: [
+      'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg',
+      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg',
+    ],
+  },
 };
 
 module.exports = nextConfig;
