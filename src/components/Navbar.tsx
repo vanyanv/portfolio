f60986834bdec8b1be7f1ed41a27c60aaf02e2a2
Vardan <vardan.vanyan@gmail.com>
2023-10-25 12:26:30 -0700

    added download icon next to resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,8 +21,37 @@ const navigation = [
   },
 ];
 
-const resumeDownload = (item: { pdf: string }) => {
-  window.open(item.pdf);
+const resumeDownloadIcon = (item: {
+  name: string;
+  href: string;
+  pdf?: string;
+}) => {
+  if (item.name === 'Resume') {
+    return (
+      <>
+        <a
+          key={item.name}
+          href={item.href}
+          download={item.pdf}
+          className='flex flex-row gap-1 text-sm font-semibold leading-6 text-gray-900 hover:animate-bounce'
+        >
+          <ArrowDownCircleIcon className='-mr-0.5 h-6 w-5' aria-hidden='true' />
+          {item.name}
+        </a>
+      </>
+    );
+  } else {
+    return (
+      <a
+        key={item.name}
+        href={item.href}
+        download={item.pdf}
+        className='text-sm font-semibold leading-6 text-gray-900 hover:animate-bounce'
+      >
+        {item.name}
+      </a>
+    );
+  }
 };
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -50,16 +79,7 @@ export default function Navbar() {
           </div>
 
           <div className='hidden lg:flex lg:gap-x-12'>
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                download={item.pdf}
-                className='text-sm font-semibold leading-6 text-gray-900 hover:animate-bounce'
-              >
-                {item.name}
-              </a>
-            ))}
+            {navigation.map((item) => resumeDownloadIcon(item))}
           </div>
 
           <div className='hidden lg:flex lg:flex-1 lg:justify-end'></div>
@@ -89,15 +109,7 @@ export default function Navbar() {
             <div className='mt-6 flow-root'>
               <div className='-my-6 divide-y divide-gray-500/10'>
                 <div className='space-y-2 py-6'>
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 '
-                    >
-                      {item.name}
-                    </a>
-                  ))}
+                  {navigation.map((item) => resumeDownloadIcon(item))}
                 </div>
                 <div className='py-6'></div>
               </div>
