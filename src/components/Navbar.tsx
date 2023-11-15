abcc9f86f41680b7913f2b082007ef44a93e84a9
Vardan <vardan.vanyan@gmail.com>
2023-11-15 11:30:15 -0800

    fixed mobile menu not closing when clicking on links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -147,6 +147,7 @@ export default function Navbar() {
                       key={uuidv4()}
                       href={item.href}
                       className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-400'
+                      onClick={() => setMobileMenuOpen(false)}
                     >
                       {item.name}
                     </a>
