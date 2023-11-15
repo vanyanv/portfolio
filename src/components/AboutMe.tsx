df1a0a031fc3e3adad6f4a79ac6554623df30ab3
Vardan <vardan.vanyan@gmail.com>
2023-11-14 23:41:07 -0800

    Finished MVP About me page

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Vardan from '/public/Images/Vardan.png';
+import Reveal from './Reveal';
 export default function AboutMe() {
   return (
     <div className='bg-white dark:bg-black h-screen'>
@@ -10,43 +11,46 @@ export default function AboutMe() {
         />
         <div className='mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8'>
           <div className='mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8'>
-            <div className='mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1'>
-              <p className='text-md leading-8 text-gray-600'>
-                Hi, I&apos;m Vardan. My world revolves around frontend
-                development, where I wield TypeScript and JavaScript to breathe
-                life into clean and beautiful user interfaces. Witnessing
-                creations come to life is what fuels my passion.
-              </p>
-              <p className='text-md leading-8 text-gray-600'>
-                Though my heart lies in the frontend, I&apos;m no stranger to
-                the backend. I&apos;ve delved into the realms of Node/Express
-                and worked with SQL/NoSQL databases to round out my skill set.
-              </p>
-              <p className='text-md leading-8 text-gray-600'>
-                My journey in technology began when my mom handed me my first
-                computer. Since then, my enthusiasm for tech has been
-                unwavering. I&apos;ve always been a hands-on explorer, diving
-                into challenges to fix and enhance things. I distinctly recall
-                upgrading my first GPU to handle games like ConquerOnline and
-                RuneScape, which were my gaming staples at the time.
-              </p>
-              <p className='text-md leading-8 text-gray-600'>
-                This passion drove me deeper into the world of computers.
-                Building them was just the beginning; I ventured into creating
-                things on the computer using HTML/CSS. Although a chapter of my
-                life led me to pursue a degree in Biology, my love for tech
-                ultimately beckoned me back to the captivating world of software
-                engineering.
-              </p>
-            </div>
-
-            <Image
-              src={Vardan}
-              alt='Creaters Image'
-              className='mt-10 aspect-[9/10]  rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-15'
-              width={500}
-              height={500}
-            />
+            <Reveal>
+              <div className='mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1'>
+                <p className='text-md leading-8 text-gray-600 dark:text-gray-300'>
+                  Hi, I&apos;m Vardan. My world revolves around frontend
+                  development, where I wield TypeScript and JavaScript to
+                  breathe life into clean and beautiful user interfaces.
+                  Witnessing creations come to life is what fuels my passion.
+                </p>
+                <p className='text-md leading-8 text-gray-600  dark:text-gray-300'>
+                  Though my heart lies in the frontend, I&apos;m no stranger to
+                  the backend. I&apos;ve delved into the realms of Node/Express
+                  and worked with SQL/NoSQL databases to round out my skill set.
+                </p>
+                <p className='text-md leading-8 text-gray-600  dark:text-gray-300'>
+                  My journey in technology began when my mom handed me my first
+                  computer. Since then, my enthusiasm for tech has been
+                  unwavering. I&apos;ve always been a hands-on explorer, diving
+                  into challenges to fix and enhance things. I distinctly recall
+                  upgrading my first GPU to handle games like ConquerOnline and
+                  RuneScape, which were my gaming staples at the time.
+                </p>
+                <p className='text-md leading-8 text-gray-600  dark:text-gray-300'>
+                  This passion drove me deeper into the world of computers.
+                  Building them was just the beginning; I ventured into creating
+                  things on the computer using HTML/CSS. Although a chapter of
+                  my life led me to pursue a degree in Biology, my love for tech
+                  ultimately beckoned me back to the captivating world of
+                  software engineering.
+                </p>
+              </div>
+            </Reveal>
+            <Reveal>
+              <Image
+                src={Vardan}
+                alt='Creaters Image'
+                className='mt-10 aspect-[9/10]  rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-15'
+                width={500}
+                height={500}
+              />
+            </Reveal>
           </div>
         </div>
       </div>
