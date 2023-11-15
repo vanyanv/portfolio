b8c6b26e88c25ed174649b7f0d500f86eb854adc
Vardan <vardan.vanyan@gmail.com>
2023-11-15 11:17:44 -0800

    minor fixes with about me page

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 export default function HomeButton() {
   return (
     <div>
-      <Link href='/'>
-        <button
-          type='button'
-          className='fixed bottom-5 right-5 inline-flex items-center gap-x-2 rounded-md bg-indigo-300 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 '
-        >
-          Home
-        </button>
-      </Link>
+      <button
+        type='button'
+        className='fixed bottom-5 right-5 inline-flex items-center gap-x-2 rounded-md bg-indigo-300 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 '
+      >
+        Home
+      </button>
     </div>
   );
 }
