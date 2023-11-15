b8178b303d06f7c3570565fc3c0556f8e7a4bfcf
Vardan <vardan.vanyan@gmail.com>
2023-11-14 23:36:00 -0800

    added profile photo

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Vardan from '/public/Images/Vardan.png';
 export default function AboutMe() {
   return (
     <div className='bg-white dark:bg-black h-screen'>
@@ -9,31 +11,41 @@ export default function AboutMe() {
         <div className='mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8'>
           <div className='mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8'>
             <div className='mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1'>
-              <p className='text-lg leading-8 text-gray-600'>
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt
-                sunt. Qui irure qui lorem cupidatat commodo.
+              <p className='text-md leading-8 text-gray-600'>
+                Hi, I&apos;m Vardan. My world revolves around frontend
+                development, where I wield TypeScript and JavaScript to breathe
+                life into clean and beautiful user interfaces. Witnessing
+                creations come to life is what fuels my passion.
               </p>
-              <div className='mt-10 flex items-center gap-x-6'>
-                <a
-                  href='#'
-                  className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                >
-                  Get started
-                </a>
-                <a
-                  href='#'
-                  className='text-sm font-semibold leading-6 text-gray-900'
-                >
-                  Learn more <span aria-hidden='true'>→</span>
-                </a>
-              </div>
+              <p className='text-md leading-8 text-gray-600'>
+                Though my heart lies in the frontend, I&apos;m no stranger to
+                the backend. I&apos;ve delved into the realms of Node/Express
+                and worked with SQL/NoSQL databases to round out my skill set.
+              </p>
+              <p className='text-md leading-8 text-gray-600'>
+                My journey in technology began when my mom handed me my first
+                computer. Since then, my enthusiasm for tech has been
+                unwavering. I&apos;ve always been a hands-on explorer, diving
+                into challenges to fix and enhance things. I distinctly recall
+                upgrading my first GPU to handle games like ConquerOnline and
+                RuneScape, which were my gaming staples at the time.
+              </p>
+              <p className='text-md leading-8 text-gray-600'>
+                This passion drove me deeper into the world of computers.
+                Building them was just the beginning; I ventured into creating
+                things on the computer using HTML/CSS. Although a chapter of my
+                life led me to pursue a degree in Biology, my love for tech
+                ultimately beckoned me back to the captivating world of software
+                engineering.
+              </p>
             </div>
-            <img
-              src='https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80'
-              alt=''
-              className='mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36'
+
+            <Image
+              src={Vardan}
+              alt='Creaters Image'
+              className='mt-10 aspect-[9/10]  rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-15'
+              width={500}
+              height={500}
             />
           </div>
         </div>
