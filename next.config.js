a417c89e59cc9598d36c30a4405970b7a250cebf
Vardan <vardan.vanyan@gmail.com>
2023-11-15 10:29:39 -0800

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@
  */
 const nextConfig = {
   images: {
-    domains: [
-      'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg',
-      'https://upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          'upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org/wikipedia/commons/f/f5/Typescript.svg',
+      },
     ],
   },
 };
